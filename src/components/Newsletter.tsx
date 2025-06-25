57c0ed4645555c2ac42a9105b198446e7cc974ce
HighLevelScaling <kian@highlevelscaling.com>
2025-06-25 16:38:24 -0700

    Fix syntax error in Newsletter component

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -28,7 +28,7 @@ const Newsletter = () => {
     <section className="py-20 bg-gradient-to-br from-red-600 via-red-500 to-orange-500 relative overflow-hidden">
       {/* Background Pattern */}
       <div className="absolute inset-0 opacity-10">
-        <div className="absolute top-0 left-0 w-full h-full bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%23ffffff\" fill-opacity=\"1\"%3E%3Ccircle cx=\"7\" cy=\"7\" r=\"2\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
+        <div className="absolute top-0 left-0 w-full h-full bg-[url('data:image/svg+xml,%3Csvg width=\\\"60\\\" height=\\\"60\\\" viewBox=\\\"0 0 60 60\\\" xmlns=\\\"http://www.w3.org/2000/svg\\\"%3E%3Cg fill=\\\"none\\\" fill-rule=\\\"evenodd\\\"%3E%3Cg fill=\\\"%23ffffff\\\" fill-opacity=\\\"1\\\"%3E%3Ccircle cx=\\\"7\\\" cy=\\\"7\\\" r=\\\"2\\\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
       </div>
 
       <div className="container mx-auto px-6 relative">
